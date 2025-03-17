f68403668d50e5fff96490969fa7cd8493b1b57d
ferclima05 <fernandocl3@al.insper.edu.br>
2025-03-17 18:28:36 +0000

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -6,10 +6,18 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile int flag_LED = 0;
+volatile int g_timer_0 = 0;
+
+bool timer_0_callback(repeating_timer_t *rt) {
+    g_timer_0 = 1;
+    flag_LED = !flag_LED;
+    return true; // keep repeating
+}
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
-        flag_f_r = 1;
+        flag_f_r = !flag_f_r;
     } else if (events == 0x8) { // rise edge
     }
 }
@@ -23,13 +31,22 @@ int main() {
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
 
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
+    int timer_0_hz = 2;
+    repeating_timer_t timer_0;
+
+    if (!add_repeating_timer_us(1000000 / timer_0_hz, 
+                                timer_0_callback,
+                                NULL, 
+                                &timer_0)) {
+        printf("Failed to add timer\n");
+    }
 
     while (true) {
-
+        
         if (flag_f_r) {
-            flag_f_r = 0;
+            gpio_put(LED_PIN_R, flag_LED);
         }
     }
 }
