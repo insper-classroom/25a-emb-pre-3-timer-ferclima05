87807b608e8b28d303fe583e9f4bfcff1854dc7e
ferclima05 <fernandocl3@al.insper.edu.br>
2025-03-17 20:14:38 +0000

    correção ex1

--- exe1/main.c
+++ exe1/main.c
@@ -1,5 +1,6 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
@@ -9,21 +10,23 @@ volatile int flag_f_r = 0;
 volatile int flag_LED = 0;
 volatile int g_timer_0 = 0;
 
-bool timer_0_callback(repeating_timer_t *rt) {
+bool timer_0_callback([[maybe_unused]] repeating_timer_t *rt) {
     g_timer_0 = 1;
     flag_LED = !flag_LED;
-    return true; // keep repeating
+    return true;
 }
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        flag_f_r = !flag_f_r;
-    } else if (events == 0x8) { // rise edge
+    if (gpio == BTN_PIN_R) {
+        if (events & GPIO_IRQ_EDGE_FALL) { 
+            flag_f_r = !flag_f_r;
+        }
     }
 }
 
 int main() {
     stdio_init_all();
+    
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
@@ -36,15 +39,11 @@ int main() {
     int timer_0_hz = 2;
     repeating_timer_t timer_0;
 
-    if (!add_repeating_timer_us(1000000 / timer_0_hz, 
-                                timer_0_callback,
-                                NULL, 
-                                &timer_0)) {
+    if (!add_repeating_timer_us(1000000 / timer_0_hz, timer_0_callback, NULL, &timer_0)) {
         printf("Failed to add timer\n");
     }
 
     while (true) {
-        
         if (flag_f_r) {
             gpio_put(LED_PIN_R, flag_LED);
         }
